15cd1f298639528282d6823e5eebcbd6c581118e
Juuso Korhonen <jk.github@turqoosi.net>
2013-12-22 19:12:39 +0200

    Learned to work with templates.

---
+++ bootstrap-test/templates/schema.sql
@@ -0,0 +1,13 @@
+drop table if exists monkeys;
+create table monkeys (
+	monkeyid integer primary key autoincrement,
+	username text not null,
+	name text not null
+);
+drop table if exists friendships;
+create table friendships (
+	nodeid integer primary key autoincrement,
+	monkeyid1 integer not null,
+	monkeyid2 integer not null
+);
+
